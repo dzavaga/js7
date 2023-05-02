479df4aedfe635ef839dbc57a4973dd025d696a0
cipher <mishaofis86@gmail.com>
2023-05-02 22:25:54 +0300

    1

---
+++ main.js
@@ -0,0 +1,21 @@
+//1
+/*
+const myWindow = window.open('', '', 'width=300,height=300');
+setTimeout(() => {
+  if (myWindow) {
+    myWindow.resizeTo(500, 500);
+  }
+}, 2000);
+setTimeout(() => {
+  if (myWindow) {
+    myWindow.moveTo(200, 200);
+  }
+}, 4000);
+setTimeout(() => {
+  if (myWindow) {
+    myWindow.close();
+  }
+}, 6000);*/
+//2
+
+
